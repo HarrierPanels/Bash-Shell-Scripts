77312d78b28718d71385d26bda82b830c837cecd
HarrierPanels <108696009+HarrierPanels@users.noreply.github.com>
2022-08-16 18:40:40 +0300

    Update case.sh
    case usage example

--- scripts/case.sh
+++ scripts/case.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # case usage
-# export PATH="~/:$PATH"
+# export PATH="~/:$PATH" ## current session path to where the file is located
 
 case $1 in
 start)
@@ -10,14 +10,14 @@ sleep 9999
 ;;
 stop)
 echo "Stopped by [PID]:"
-echo "$(ps aux | grep case.sh | grep -v grep | awk '{print $2}')"
+ps aux | grep case.sh | grep -v grep | awk '{print $2}'
 killall sleep 2>/dev/null
 exit 143
 #kill $(ps aux | grep case.sh | grep -v grep | awk '{print $2}')
 ;;
 restart)
 echo "Stopped by [PID]:"
-echo "$(ps aux | grep case.sh | grep -v grep | awk '{print $2}')"
+ps aux | grep case.sh | grep -v grep | awk '{print $2}'
 killall sleep 2>/dev/null
 #kill $(ps aux | grep sleep | grep -v grep | awk '{print $2}')
 case.sh start
