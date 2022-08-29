e8d035842f5a18257370f8b724eec114bba67bdb
HarrierPanels <108696009+HarrierPanels@users.noreply.github.com>
2022-08-29 16:48:03 +0300

    Add files via upload

---
+++ scripts/Konsole.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Konsole Emulation v.1
+# Supports commands: ls, pwd, hi
+
+hn=`hostname`
+usr=`whoami`
+if dir="$HOME"; then
+dir="~"
+else
+dir="${PWD##*/}"
+fi
+
+cmd="[$usr"@"$hn $dir]$ "
+   
+while :; do 
+
+IFS= read -r -p "$cmd" input
+
+case $input in
+
+ls) echo "$($input)" | tr ':' ':\n';;
+"ls "*) echo "$($input)" | tr ':' ':\n';;
+pwd) pwd;;
+[Hh][i]) echo "hi $usr!";;
+exit) exit;;
+"");;
+*) echo "bash: "$input": command not found";;
+esac 
+done
---
+++ scripts/Konsole_a.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Konsole Emulation v.1.a
+# Supports commands: ls, pwd, hi
+
+hn=`hostname`
+usr=`whoami`
+if dir="$HOME"; then
+dir="~"
+else
+dir="${PWD##*/}"
+fi
+cmd="[$usr"@"$hn $dir]$ "
+
+echo "$cmd"
+   
+while IFS= read -r -a input; do
+echo "$cmd"
+
+case ${input[@]} in
+
+ls)
+echo "$(${input[@]})" | tr ':' ':\n'
+echo "$cmd"
+;;
+"ls "*) 
+echo "$(${input[@]})" | tr ':' ':\n'
+echo "$cmd"
+;;
+pwd) pwd
+echo "$cmd"
+;;
+[Hh][i] ) echo "hi $usr!"
+echo "$cmd"
+;;
+exit) exit;;
+"")
+;;
+*)
+echo "bash: "${input[@]}": command not found"
+#echo "bash: "$input": command not found";;
+echo "$cmd"
+;;
+esac 
+done
