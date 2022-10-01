001c66682d797694c08a28bdeb665875bd9449d6
HarrierPanels <108696009+HarrierPanels@users.noreply.github.com>
2022-10-02 00:41:46 +0300

    Update a.sh

--- scripts/misc/a.sh
+++ scripts/misc/a.sh
@@ -1,5 +1,9 @@
 bar() {
-    for i in $2; do printf "$i" && sleep 0.1; done
+for j in $*; do 
+printf "$j" 
+sleep 0.1
+done
+    
 }
 
 emu() {
@@ -17,5 +21,39 @@ sleep 0.1
 done
 }
 
+emu2() {
+for j in $2; do 
+printf "$j" 
+sleep 0.1
+done
 
-emu "/ | \\ -" " L o a d i n g  . . ."
+sleep 5 &
+pid=$!
+while kill -0 $pid >/dev/null 2>&1; do 
+for i in $1; do
+printf "\r$i " 
+
+sleep 0.1
+done
+#printf "$j" 
+#sleep 0.1
+done
+}
+
+emu3() {
+
+bar $2
+
+sleep 5 &
+pid=$!
+
+while kill -0 $pid >/dev/null 2>&1; do 
+for i in $1; do
+printf "\r      $i" 
+sleep 0.1
+done
+done
+
+}
+#emu "/ | \\ -" " L o a d i n g . . ."
+emu3 "/ | \\ -" "\tL o a d i n g . . ."
